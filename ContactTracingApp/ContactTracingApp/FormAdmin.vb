6cb70152fb2c0d91c129c37d2d66fa16a6c26c74
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-06-19 22:42:04 +0800

    Added new form called FormAdminthat serves as the form for Contact Tracing Data Viewer.

---
+++ ContactTracingApp/ContactTracingApp/FormAdmin.vb
@@ -0,0 +1,3 @@
+﻿Public Class FormAdmin
+
+End Class
--- ContactTracingApp/ContactTracingApp/FormAdminLogin.vb
+++ ContactTracingApp/ContactTracingApp/FormAdminLogin.vb
@@ -12,8 +12,10 @@
 
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
         If txtBxPassword.Text = "123" Then
-            formMain.Close()
-            Me.Close()
+            Dim formAdmin As FormAdmin = New FormAdmin
+            formMain.Hide()
+            Me.Hide()
+            formAdmin.Show()
         Else
             MessageBox.Show("wrong password!")
         End If
