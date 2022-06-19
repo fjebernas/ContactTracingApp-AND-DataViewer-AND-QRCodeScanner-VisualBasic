00dc2a51421d107b93ac72edd934d7169daa5057
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-06-20 02:47:13 +0800

    Added controls needed to display the user's details on the panelContent of FormAdmin.

--- ContactTracingApp/ContactTracingApp/FormAdmin.Designer.vb
+++ ContactTracingApp/ContactTracingApp/FormAdmin.Designer.vb
@@ -22,61 +22,64 @@ Partial Class FormAdmin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Panel3 = New System.Windows.Forms.Panel()
-        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.panelTop = New System.Windows.Forms.Panel()
+        Me.panelBottom = New System.Windows.Forms.Panel()
+        Me.panelNav = New System.Windows.Forms.Panel()
+        Me.panelContent = New System.Windows.Forms.Panel()
         Me.listBxEntries = New System.Windows.Forms.ListBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Panel1.SuspendLayout()
-        Me.Panel3.SuspendLayout()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.panelTop.SuspendLayout()
+        Me.panelNav.SuspendLayout()
+        Me.panelContent.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Panel1
+        'panelTop
         '
-        Me.Panel1.BackColor = System.Drawing.SystemColors.ControlDark
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Panel1.Location = New System.Drawing.Point(0, 0)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(944, 85)
-        Me.Panel1.TabIndex = 0
+        Me.panelTop.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.panelTop.Controls.Add(Me.Label1)
+        Me.panelTop.Dock = System.Windows.Forms.DockStyle.Top
+        Me.panelTop.Location = New System.Drawing.Point(0, 0)
+        Me.panelTop.Name = "panelTop"
+        Me.panelTop.Size = New System.Drawing.Size(944, 85)
+        Me.panelTop.TabIndex = 0
         '
-        'Panel2
+        'panelBottom
         '
-        Me.Panel2.BackColor = System.Drawing.SystemColors.ControlDark
-        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Panel2.Location = New System.Drawing.Point(0, 492)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(944, 48)
-        Me.Panel2.TabIndex = 1
+        Me.panelBottom.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.panelBottom.Location = New System.Drawing.Point(0, 492)
+        Me.panelBottom.Name = "panelBottom"
+        Me.panelBottom.Size = New System.Drawing.Size(944, 48)
+        Me.panelBottom.TabIndex = 1
         '
-        'Panel3
+        'panelNav
         '
-        Me.Panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.Panel3.Controls.Add(Me.listBxEntries)
-        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Left
-        Me.Panel3.Location = New System.Drawing.Point(0, 85)
-        Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(265, 407)
-        Me.Panel3.TabIndex = 2
+        Me.panelNav.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.panelNav.Controls.Add(Me.listBxEntries)
+        Me.panelNav.Dock = System.Windows.Forms.DockStyle.Left
+        Me.panelNav.Location = New System.Drawing.Point(0, 85)
+        Me.panelNav.Name = "panelNav"
+        Me.panelNav.Size = New System.Drawing.Size(265, 407)
+        Me.panelNav.TabIndex = 2
         '
-        'Panel4
+        'panelContent
         '
-        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel4.Location = New System.Drawing.Point(265, 85)
-        Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(679, 407)
-        Me.Panel4.TabIndex = 3
+        Me.panelContent.Controls.Add(Me.Label2)
+        Me.panelContent.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.panelContent.Location = New System.Drawing.Point(265, 85)
+        Me.panelContent.Name = "panelContent"
+        Me.panelContent.Size = New System.Drawing.Size(679, 407)
+        Me.panelContent.TabIndex = 3
         '
         'listBxEntries
         '
-        Me.listBxEntries.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.listBxEntries.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.listBxEntries.FormattingEnabled = True
-        Me.listBxEntries.ItemHeight = 21
+        Me.listBxEntries.ItemHeight = 17
         Me.listBxEntries.Location = New System.Drawing.Point(23, 44)
         Me.listBxEntries.Name = "listBxEntries"
-        Me.listBxEntries.Size = New System.Drawing.Size(220, 340)
+        Me.listBxEntries.Size = New System.Drawing.Size(220, 344)
         Me.listBxEntries.TabIndex = 0
         '
         'Label1
@@ -89,29 +92,42 @@ Partial Class FormAdmin
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Contact Tracing Data Viewer"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(34, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 21)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Label2"
+        '
         'FormAdmin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(944, 540)
-        Me.Controls.Add(Me.Panel4)
-        Me.Controls.Add(Me.Panel3)
-        Me.Controls.Add(Me.Panel2)
-        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.panelContent)
+        Me.Controls.Add(Me.panelNav)
+        Me.Controls.Add(Me.panelBottom)
+        Me.Controls.Add(Me.panelTop)
         Me.Name = "FormAdmin"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Contact Tracing Data Viewer"
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
-        Me.Panel3.ResumeLayout(False)
+        Me.panelTop.ResumeLayout(False)
+        Me.panelTop.PerformLayout()
+        Me.panelNav.ResumeLayout(False)
+        Me.panelContent.ResumeLayout(False)
+        Me.panelContent.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents Panel2 As Panel
-    Friend WithEvents Panel3 As Panel
-    Friend WithEvents Panel4 As Panel
+    Friend WithEvents panelTop As Panel
+    Friend WithEvents panelBottom As Panel
+    Friend WithEvents panelNav As Panel
+    Friend WithEvents panelContent As Panel
     Friend WithEvents Label1 As Label
     Friend WithEvents listBxEntries As ListBox
+    Friend WithEvents Label2 As Label
 End Class
